3d5d032a2bd937ef2680be97cbc7cf375fb9270c
StevenMadali <stevenmadali17@gmail.com>
2024-07-09 22:51:10 +0800

    add customization to bootstrap navbar

--- docs/Documentation.md
+++ docs/Documentation.md
@@ -3,3 +3,4 @@ STUDENT MANAGEMENT CREATION DOCUMENTATION
 1.get boostrap css links and js links
 2.setup background image
 3.get navbar code to bootstrap
+4.customize navbar
--- index.php
+++ index.php
@@ -45,19 +45,35 @@
         <!-- NAVBAR -->
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="./images/cvsulogo.png"></a>
+    <a class="navbar-brand" href="#"><img src="./images/cvsulogo.png" width="30"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">About</a>
         </li>
-        <li class="nav-item dropdown">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        </ul>
+
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        </ul>
+
+            <!-- DON'T NEED UNLESS NECCESSARY UNCOMMENT! -->
+        <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
@@ -70,12 +86,16 @@
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
+        </li> -->
+
+
+
+      <!-- <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> -->
+
+
     </div>
   </div>
 </nav>
