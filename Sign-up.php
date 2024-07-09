7c1e8990c6b9726ead0ed94df8a340af41b7dfd2
StevenMadali <stevenmadali17@gmail.com>
2024-07-09 22:24:23 +0800

    first revision to cvsu school management system

--- Sign-up.php
+++ Sign-up.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign-Up</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="shortcut icon" href="./images/cvsulogo.png" type="image/x-icon">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,11 +13,149 @@
 </head>
 <body>
 
-<!-- <style>
-  .error {
+<!-- OWN CSS IS HERE! -->
+
+<style>
+    body{
+    margin: 0;
+    padding: 0;
+    background-image: url(./images/bg.jpg);
+    background-position: center center; 
+     background-repeat: no-repeat; 
+    background-size: cover;
+    width: 100%; 
+    height: auto; 
+}
+
+.error {
     color: red;
-  }
-</style> -->
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+}
+.slidetitle{
+
+    margin-left: 10%;
+}
+.titlecard {
+    color: white;
+    font-size: 1.9rem;
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+}
+.subtitle {
+    margin-top: 35%;
+    color: white;
+    font-size: 1.9rem;
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+}
+
+.signup_container {
+    float: right;
+    background: white;
+    height: 75vh;
+    width: 20vw;
+    margin-top: 3%;
+    margin-right: 5%;
+    border-radius: 5%;
+    padding: 1%;
+    padding: 2%;
+}
+
+.title{
+    text-align: center;
+    font-size: 1.5vw;
+    color: #252525;
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+}
+input{
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+    width: 100%;
+    background-color: beige;
+    border-radius: 3px;
+}
+label{
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+}
+img {
+    height: 4.4vw;
+    width: 4.9vw;
+}
+
+
+.divemail {
+    margin-top: 5%;
+    margin-bottom: 1%;
+    font-family: "Righteous", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: #252525;
+    width: 100%;
+}
+.labelemail{
+    width: 100%;
+}
+
+
+.divusern {
+    margin-top: 5%;
+    margin-bottom: 1%;
+    font-family: "Righteous", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: #252525;
+}
+.divpass {
+    margin-top: 5%;
+    margin-bottom: 1%;
+    font-family: "Righteous", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: #252525;
+}
+
+/* .divsub {
+    margin-top: 7%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    width: 100%;
+} */
+
+.subbtn {
+    margin-top: 7%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    width: 100%;
+    --color: #00A97F;
+    padding: 0.8em 1.7em;
+    background-color: transparent;
+    border-radius: .3em;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+    transition: .5s;
+    font-weight: 400;
+    font-size: 17px;
+    border: 1px solid;
+    font-family: inherit;
+    text-transform: uppercase;
+    color: var(--color);
+    z-index: 1;
+   }
+   
+ 
+</style>
 
 
 <?php
---
+++ docs/Documentation.md
@@ -0,0 +1,3 @@
+STUDENT MANAGEMENT CREATION DOCUMENTATION
+
+1.get boostrap css links and js links
