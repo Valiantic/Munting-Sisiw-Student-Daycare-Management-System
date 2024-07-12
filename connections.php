c929a29ec7238fafcaa29ef501ed3a8bfbe2ec61
StevenMadali <stevenmadali17@gmail.com>
2024-07-12 18:01:12 +0800

    add connections to req/login.php to db_daycare as well create tables for admin, students and teachers

--- connections.php
+++ connections.php
@@ -1,14 +1,29 @@
 <?php
 
-// database connection indicator
-$connections = mysqli_connect("localhost","root","","db_signup");
-    if(mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL " .mysqli_connect_error();
-    }
-    // else {
-    //     echo "connected"; indicate that the database is connected
-    // }
-   
+// // database connection indicator
+// $connections = mysqli_connect("localhost","root","","db_signup");
+//     if(mysqli_connect_errno()) {
+//         echo "Failed to connect to MySQL " .mysqli_connect_error();
+//     }
+//     // indicate that the database is connected
+//     // else {
+//     //     echo "connected"; indicate that the database is connected
+//     // }
 
 
 ?>
+
+<?php  
+
+$sName = "localhost";
+$uName = "root";
+$pass  = "";
+$db_name = "db_daycare";
+
+try {
+	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOExeption $e){
+	echo "Connection failed: ". $e->getMessage();
+	exit;
+}
--- req/login.php
+++ req/login.php
@@ -11,6 +11,9 @@ if(isset($_POST['uname']) &&
    isset($_POST['pass']) &&
    isset($_POST['role'])) {
 
+
+	include "../connections.php";
+
     $uname = $_POST['uname'];
     $pass = $_POST['pass'];
     $role = $_POST['role'];
@@ -28,12 +31,37 @@ if(isset($_POST['uname']) &&
 		header("Location: ../login.php?error=$em");
 		exit;
 	}else {
-        echo "Success";
-        // CREATE CONNECTIONS!!!! 6:48
+       
+		if($role == '1'){
+			$sql = "SELECT * FROM tbl_admin 
+					WHERE username = ?";
+			$role = "Admin";
+		}
+		else if($role == '2'){
+			$sql = "SELECT * FROM tbl_teachers
+					WHERE username = ?";
+			$role = "Teacher";
+		}else {
+			$sql = "SELECT * FROM tbl_students
+					WHERE username = ?";
+			$role = "Student";
+		}
+			$stmt = $conn->prepare($sql);
+			$stmt->execute([$uname]);
+
+			if($stmt->rowCount() == 1){
+				$user = $stmt->fetch();
+			}else {
+				$em  = "Incorrect Username or Password";
+				header("Location: ../login.php?error=$em");
+				exit;
+			}
+
     }
 
 }else {
     header("Location: ../login.php");
+	exit;
 }
 
 
